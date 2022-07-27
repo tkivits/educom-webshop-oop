0d07ad6558f5fe61babb2af307739d9a7a86501a
tkivits <t.kivits@hotmail.com>
2022-07-27 11:25:22 +0200

    Issue#12: HtmlDoc aangepast.

--- views/HtmlDoc.php
+++ views/HtmlDoc.php
@@ -2,24 +2,41 @@
 
 class HtmlDoc {
     //Methods
-    protected function showDocStart() {
+    private function showDocStart() {
         echo 
             '<!DOCTYPE html>
-            <html>
-            <head>
-            <link rel="stylesheet" href="../CSS/stylesheet.css">
-            </head>
-            <body>';
+            <html>';
     }
-    protected function showDocEnd() {
-        echo 
-            '</body>
-            </html>';
+    private function showHeadStart() {
+        echo '<head>';
+    }
+    protected function showHeadContent() {
+        echo '';
+    }
+    private function showHeadEnd() {
+        echo '</head>';
+    }
+    private function showBodyStart() {
+        echo '<body>';
+    }
+    protected function showBodyContent() {
+        echo '';
+    }
+    private function showBodyEnd() {
+        echo '</body>';
+    }
+    private function showDocEnd() {
+        echo '</html>';
     }
     public function show() {
         $this->showDocStart();
+        $this->showHeadStart();
+        $this->showHeadContent();
+        $this->showHeadEnd();
+        $this->showBodyStart();
+        $this->showBodyContent();
+        $this->showBodyEnd();
         $this->showDocEnd();
     }
 }
-
 ?>
