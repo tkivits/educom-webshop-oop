02eaa870a36e2ad19c7ed42a256efe3e92053fc2
tkivits <t.kivits@hotmail.com>
2022-07-27 11:42:16 +0200

    Issue#12: BasicDoc aangepast.

--- views/BasicDoc.php
+++ views/BasicDoc.php
@@ -7,22 +7,41 @@ class BasicDoc extends HtmlDoc {
     {
         $this->page = $page;
     }
-    protected function showHeader() {
+    private function showHeader() 
+    {
         echo '<h1 class="header">'.$this->page.'</h1>';
     }
-    protected function showMenu() {
-        include '../Pages/menu.php';
+    private function showMenu() 
+    {
+        echo '<ul class="menu">';
+        echo '<li><a href="?page=Home">Home</a></li>';
+        echo '<li><a href="?page=About">About</a></li>';
+        echo '<li><a href="?page=Contact">Contact</a></li>';
+        echo '<li><a href="?page=Webshop">Webshop</a></li>';
+        if (!isset($_SESSION['login'])) {
+            echo '<li><a href="?page=Register">Register</a></li>';
+            echo '<li><a href="?page=Login">Login</a></li>';
+        } else {
+            echo '<li><a href="?page=Cart">Cart</a></li>';
+            echo '<li><a href="?page=Logout">Logout '.$_SESSION['name'].'</a></li>';
+        }
+        echo '</u>';
     }
-    protected function showFooter() {
-        include '../Pages/footer.php';
+    private function showFooter() 
+    {
+        echo '<footer class="foot"><p>&copy; 2022 Teun Kivits</p></footer>';
     }
-    public function show(){
-        $this->showDocStart();
+    protected function showContent() 
+    {
+        echo '';
+    }
+    protected function showBodyContent()
+    {
         $this->showHeader();
         $this->showMenu();
+        $this->showContent();
         $this->showFooter();
-        $this->showDocEnd();
     }
-}
 
+}
 ?>
