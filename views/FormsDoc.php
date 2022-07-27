d7d4c87857d347e201b26d1bf995f7cefa49490e
tkivits <t.kivits@hotmail.com>
2022-07-27 12:31:08 +0200

    Issue#4: FormsDoc toegevoegd.

--- views/FormsDoc.php
+++ views/FormsDoc.php
@@ -2,5 +2,9 @@
 require_once 'BasicDoc.php';
 
 abstract class FormsDoc extends BasicDoc {
+    abstract protected function showFormStart();
+    abstract protected function showEmailInput();
+    abstract protected function showSubmitButton();
+    abstract protected function showFormEnd();
 }
 ?>
