712731688614b191b859da34d4a6b11e8619a73f
tkivits <t.kivits@hotmail.com>
2022-07-28 12:43:32 +0200

    Issue#4: ProductDoc.php aangepast.

--- views/ProductDoc.php
+++ views/ProductDoc.php
@@ -2,8 +2,18 @@
 require_once 'BasicDoc.php';
 abstract class ProductDoc extends BasicDoc 
 {
-    abstract protected function getProduct();
-    abstract protected function showProduct();
+    public function __construct($page, $productid, $productname, $productprice, $productdescription, $productimage,)
+    {
+        $this->page = $page;
+        $this->id = $productid;
+        $this->name = $productname;
+        $this->price = $productprice;
+        $this->description = $productdescription;
+        $this->image = $productimage;
+    }
+    abstract protected function showProductImage();
+    abstract protected function showProductName();
+    abstract protected function showProductPrice();
     abstract protected function showAddToCartButton();
 }
 ?>
