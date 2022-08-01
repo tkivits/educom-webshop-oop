be9135fda5da5dfe109c95b5e87e09058ff2d50e
tkivits <t.kivits@hotmail.com>
2022-08-01 11:31:48 +0200

    Issue#6: Basis van PageController toegevoegd.

---
+++ controllers/PageControllerClass.php
@@ -0,0 +1,38 @@
+<?php
+
+class PageController
+{
+    private $model;
+
+    public function __construct()
+    {
+        this->model = new PageModel();
+    }
+    public function handleRequest()
+    {
+        $this->getRequest();
+        $this->showResponse();
+    }
+    private function getRequest()
+    {
+        $this->model->getRequestedPage();
+    }
+    private function showResponse()
+    {
+        switch($this->model->page)
+        {
+            case 'Home';
+            include_once "views/HomeDoc.php";
+		  	$view = new HomeDoc($this->model->page);
+		  	$view->show();
+		  	break;
+            case 'About';
+            include_once "views/AboutDoc.php";
+		  	$view = new AboutDoc($this->model->page);
+		  	$view->show();
+		  	break;
+        }
+    }
+}
+
+?>
