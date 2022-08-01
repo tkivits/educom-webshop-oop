d33bcf100966b7825d5f8834e723565ae4a2c13f
tkivits <t.kivits@hotmail.com>
2022-08-01 12:52:14 +0200

    Issue#6: BasicDoc aangepast.

--- models/PageModelClass.php
+++ models/PageModelClass.php
@@ -16,6 +16,39 @@ class PageModel
             $this->page = $_GET['page'];
         }
     }
+    public function showHeader() 
+    {
+        echo '<h1 class="header">'.$this->page.'</h1>';
+    }
+    public function createMenu()
+    {
+        $this->showMenuStart();
+        $this->showMenuItem('Home');
+        $this->showMenuItem('About');
+        //$this->showMenuItem('Contact');
+        //$this->showMenuItem('Webshop');
+        //geen login
+        //$this->showMenuItem('Register');
+        //$this->showMenuItem('Login');
+        //wel login
+        //$this->showMenuItem('Cart');
+        //$this->showMenuItem('Logout', 'username');
+        $this->showMenuEnd();
+    }
+    private function showMenuStart()
+    {
+        echo '<div>';
+        echo '<ul class="menu">';
+    }
+    private function showMenuItem($name, $optional = NULL)
+    {
+        echo '<li><a href="?page='.$name.'">'.$name.' '.$optional.'</a></li>';
+    }
+    private function showMenuEnd()
+    {
+        echo '</u>';
+        echo '</div>';
+    }
     public function showHomeContent()
     {
         echo 'Welcome on this site!';
@@ -31,6 +64,10 @@ class PageModel
                 </ul>
              Recently, I've started a traineeship at Educom where i'm learning to build websites like this one. Hopefully this will become a functional webshop!<br>
              Right now i'm still learning though, as you can clearly see on this site."; 
+    }
+    public function showFooter() 
+    {
+        echo '<footer class="foot"><p>&copy; 2022 Teun Kivits</p></footer>';
     }
 }
 
--- views/BasicDoc.php
+++ views/BasicDoc.php
@@ -8,43 +8,16 @@ class BasicDoc extends HtmlDoc
     {
         $this->model = $model;
     }
-    private function showHeader() 
-    {
-        echo '<h1 class="header">'.$this->model->page.'</h1>';
-    }
-    private function showMenu() 
-    {
-        echo '<div>';
-        echo '<ul class="menu">';
-        echo '<li><a href="?page=Home">Home</a></li>';
-        echo '<li><a href="?page=About">About</a></li>';
-        echo '<li><a href="?page=Contact">Contact</a></li>';
-        echo '<li><a href="?page=Webshop">Webshop</a></li>';
-        if (!isset($_SESSION['login'])) {
-            echo '<li><a href="?page=Register">Register</a></li>';
-            echo '<li><a href="?page=Login">Login</a></li>';
-        } else {
-            echo '<li><a href="?page=Cart">Cart</a></li>';
-            echo '<li><a href="?page=Logout">Logout '.$_SESSION['name'].'</a></li>';
-        }
-        echo '</u>';
-        echo '</div>';
-    }
-    private function showFooter() 
-    {
-        echo '<footer class="foot"><p>&copy; 2022 Teun Kivits</p></footer>';
-    }
     protected function showContent() 
     {
         echo '';
     }
     protected function showBodyContent()
     {
-        $this->showHeader();
-        $this->showMenu();
+        $this->model->showHeader();
+        $this->model->createMenu();
         $this->showContent();
-        $this->showFooter();
+        $this->model->showFooter();
     }
-
 }
 ?>
