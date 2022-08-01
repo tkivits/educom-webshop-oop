267613d13d78e257a2eb075fd739b39838b490f8
tkivits <t.kivits@hotmail.com>
2022-08-01 16:19:35 +0200

    Issue#7: ContactDoc aangepast.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require_once('controllers/PageControllerClass.php');
+require_once('controllers/PageController.php');
 
 $controller = new PageController();
 $controller->handleRequest();
--- views/ContactDoc.php
+++ views/ContactDoc.php
@@ -2,33 +2,6 @@
 include_once 'FormsDoc.php';
 class ContactDoc extends FormsDoc
 {
-    public $page;
-    public $name;
-    public $namerr;
-    public $email;
-    public $emailerr;
-    public $phone;
-    public $phonerr;
-    public $compref;
-    public $compreferr;
-    public $mess;
-    public $messerr;
-    public function __construct($page, $sal, $salerr, $name, $namerr, $email, $emailerr, $phone, $phonerr, $compref, $compreferr, $mess, $messerr)
-    {
-        $this->page = $page;
-        $this->sal = $sal;
-        $this->salerr = $salerr;
-        $this->name = $name;
-        $this->namerr = $namerr;
-        $this->email = $email;
-        $this->emailerr = $emailerr;
-        $this->phone = $phone;
-        $this->phonerr = $phonerr;
-        $this->compref = $compref;
-        $this->compreferr = $compreferr;
-        $this->mess = $mess;
-        $this->messerr = $messerr;
-    }
     protected function showRequiredField()
     {
         echo '<div class="errordiv"><span class="error">Fields with a * are required</span></div> ';
@@ -43,45 +16,45 @@ class ContactDoc extends FormsDoc
         echo '<select id="sal" name="sal">';
         echo '<option value="">Choose</option>';
         echo '<option value="Mr."';
-        if ($this->sal == "Mr.") {echo "selected";};
+        if ($this->model->sal == "Mr.") {echo "selected";};
         echo '>Mr.</option>';
         echo '<option value="Mrs"';
-        if ($this->sal == "Mrs") {echo "selected";};
+        if ($this->model->sal == "Mrs") {echo "selected";};
         echo '>Mrs</option>';
-        echo '</select><span class="error">  * '.$this->salerr.'</span></div>';
+        echo '</select><span class="error">  * '.$this->model->salerr.'</span></div>';
     }
     private function showNameInput()
     {
-        echo '<div><label for="name">Name:</label><input type="text" id="name" name="name" value="'.$this->name.'">';
-        echo '<span class="error">  * '.$this->namerr.'</span></div>';
+        echo '<div><label for="name">Name:</label><input type="text" id="name" name="name" value="'.$this->model->name.'">';
+        echo '<span class="error">  * '.$this->model->namerr.'</span></div>';
     }
     protected function showEmailInput()
     {
-        echo '<div><label for="email">E-mail:</label><input type="email" id="email" name="email" value="'.$this->email.'">';
-        echo '<span class="error">  * '.$this->emailerr.'</span></div>';
+        echo '<div><label for="email">E-mail:</label><input type="email" id="email" name="email" value="'.$this->model->email.'">';
+        echo '<span class="error">  * '.$this->model->emailerr.'</span></div>';
     }
     private function showPhoneInput()
     {
-        echo '<div><label for="phone">Phone number:</label><input type="tel" id="phone" name="phone" value="'.$this->phone.'">';
-        echo '<span class="error">  * '.$this->phonerr.'</span></div>';
+        echo '<div><label for="phone">Phone number:</label><input type="tel" id="phone" name="phone" value="'.$this->model->phone.'">';
+        echo '<span class="error">  * '.$this->model->phonerr.'</span></div>';
     }
     private function showCommunicationPreferenceInput()
     {
         echo '<div><label for="compref">What is your communication preference?</label>';
         echo '<input type="radio" name="compref" value="E-mail"';
-        if ($this->compref == "E-mail") {echo "checked";};
+        if ($this->model->compref == "E-mail") {echo "checked";};
         echo '><label for="email">E-mail</label>';
         echo '<input type="radio" name="compref" value="Telephone"';
-        if ($this->compref == "Telephone") {echo "checked";};
+        if ($this->model->compref == "Telephone") {echo "checked";};
         echo '><label for="telephone">Telephone</label>';
-        echo '<span class="error">  * '.$this->compreferr.'</span></div>';
+        echo '<span class="error">  * '.$this->model->compreferr.'</span></div>';
     }
     private function showMessageInput()
     {
         echo '<div><textarea id="mess" name="mess" rows="8" cols="50" placeholder= "Tell us why you want to contact us!">';
-        if (isset($this->mess)) {echo $this->mess;};
+        if (isset($this->model->mess)) {echo $this->model->mess;};
         echo '</textarea>';
-        echo '<span class="error">  * '.$this->messerr.'</span></div>';
+        echo '<span class="error">  * '.$this->model->messerr.'</span></div>';
     }
     protected function showSubmitButton()
     {
