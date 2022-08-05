8a07ba243e0bb10a2b7de0cbd0ea8426a6851ffe
tkivits <t.kivits@hotmail.com>
2022-08-05 16:31:25 +0200

    Issue#11: crud aangepast.

--- Crud.php
+++ Crud.php
@@ -31,6 +31,36 @@ class Crud
         }
         return $this->pdo->lastInsertId();
     }
+    public function createOrderRow($array)
+    {
+        $sql = 'INSERT INTO orders (user_id, total) VALUES (:user_id, :total)';
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+        $stmt->execute($array);
+        if (!$this->pdo->lastInsertId())
+        {
+            throw new Exception('Failed to register order.');
+        }
+        return $this->pdo->lastInsertId();
+    }
+    public function createOrderItemsRow($order_id, $array)
+    {
+        $sql = 'INSERT INTO order_item (order_id, product_id, quantity) VALUES (:order_id, :product_id, :qty)';
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+        foreach ($array as $product_id => $qty)
+        {
+            $stmt->bindValue(':order_id', $order_id);
+            $stmt->bindValue(':product_id', $product_id);
+            $stmt->bindValue(':qty', $qty);
+            $stmt->execute();
+            if (!this->pdo->lastInsertId())
+            {
+                throw new Exception('Failed to register order.');
+            }
+        }
+        return $this->pdo->lastInsertId();
+    }
     public function readOneRow($table, $row, $value)
     {
         $sql = 'SELECT * FROM '.$table.' WHERE '.$row.' = :value';
@@ -46,6 +76,15 @@ class Crud
         $sql = 'SELECT * FROM '.$table.' WHERE '.$row.' = :value';
         $stmt = $this->pdo->prepare($sql);
         $stmt->bindValue(':value', $value);
+        $stmt->setFetchMode(PDO::FETCH_OBJ);
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_OBJ);
+        return $result;
+    }
+    public function readTable($table)
+    {
+        $sql = 'SELECT * FROM '.$table;
+        $stmt = $this->pdo->prepare($sql);
         $stmt->setFetchMode(PDO::FETCH_OBJ);
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_OBJ);
