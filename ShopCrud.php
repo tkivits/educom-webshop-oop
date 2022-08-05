1699034740107d1b02e8a8ad7f84ee1e9c0ff756
tkivits <t.kivits@hotmail.com>
2022-08-05 15:39:17 +0200

    Issue#11: ShopCrud toegevoegd.

---
+++ ShopCrud.php
@@ -0,0 +1,23 @@
+<?php
+
+class UserCrud
+{
+    private $crud;
+    public function __construct($crud)
+    {
+        $this->crud = $crud;
+    }
+    public function createNewOrder()
+    {
+        //create
+    }
+    public function readSingleProduct()
+    {
+        //read
+    }
+    public function readAllProducts()
+    {
+        //read
+    }
+}
+?>
