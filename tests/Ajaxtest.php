46f139fc3a952404ed933bbce8a4d0676cea2ee3
tkivits <t.kivits@hotmail.com>
2022-08-12 15:50:11 +0200

    Issue#23: Ajaxtest aangemaakt om de MVC te testen.

---
+++ tests/Ajaxtest.php
@@ -0,0 +1,17 @@
+<?php
+$parent = dirname(dirname(__FILE__));
+require_once $parent.'/StaticUtilClass.php';
+require_once $parent.'/RatingCrud.php';
+require_once $parent.'/Crud.php';
+require_once $parent.'/controllers/AjaxController.php';
+require_once $parent.'/models/AjaxModel.php';
+require_once $parent.'/views/AjaxView.php';
+
+$crud = new Crud(null);
+$ratingCrud = new RatingCrud($crud);
+$model = new AjaxModel(null, $ratingCrud);
+$view = new AjaxDoc($model);
+
+$view->show();
+
+?>
