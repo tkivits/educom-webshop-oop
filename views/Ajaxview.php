7d0013e0661904106aee6dc94100baea497ce3e1
tkivits <t.kivits@hotmail.com>
2022-08-12 16:05:57 +0200

    Issue#23: Functie voor JSON encode toegevoegd en getest.

--- views/Ajaxview.php
+++ views/Ajaxview.php
@@ -10,13 +10,47 @@ class AjaxDoc extends BasicDoc
         echo '<br>';
         $oneRating = $this->model->getProductRatingByID('1');
         var_dump($oneRating);
+        echo '<br>';
+        echo '<br>';
+        $twoRating = $this->model->getProductRatingByID('2');
+        var_dump($twoRating);
+        echo '<br>';
+        echo '<br>';
+        $threeRating = $this->model->getProductRatingByID('3');
+        var_dump($threeRating);
+        echo '<br>';
+        echo '<br>';
+        $fourRating = $this->model->getProductRatingByID('4');
+        var_dump($fourRating);
+        echo '<br>';
+        echo '<br>';
+        $fiveRating = $this->model->getProductRatingByID('5');
+        var_dump($fiveRating);
     }
     // Encode JSON
+    protected function JsonEncodeAllRatings()
+    {
+        $input = $this->model->allRatings;
+        $json = json_encode($input);
+        var_dump($json);
+    }
+    protected function JsonEncodeOneRating()
+    {
+        $input = $this->model->getProductRatingByID('5');
+        $json = json_encode($input);
+        var_dump($json);
+    }
 
     // Decode JSON
     protected function showContent() 
     {
         $this->runTest();
+        echo '<br>';
+        echo '<br>';
+        $this->JsonEncodeAllRatings();
+        echo '<br>';
+        echo '<br>';
+        $this->JsonEncodeOneRating();
     }
 }
 
